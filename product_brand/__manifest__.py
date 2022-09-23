40438f82033c588c102bda994731921dbb8ed760
Juan Pablo Levy <operaciones@indasoge.com>
2022-09-23 16:10:37 -0300

    Product Brand Model 15

--- product_brand/__manifest__.py
+++ product_brand/__manifest__.py
@@ -11,7 +11,7 @@
 
 {
     "name": "Product Brand Manager",
-    "version": "14.0.1.2.0",
+    "version": "15.0.1.0.0",
     "development_status": "Mature",
     "category": "Product",
     "summary": "Product Brand Manager",
